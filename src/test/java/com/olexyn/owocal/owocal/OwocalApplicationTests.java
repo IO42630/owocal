8a0ead8d5069c6de01c83cfad46d90d9c71f6e0d
Ivan Olexyn <ivan@olexyn.com>
2020-12-21 01:29:21 +0100

    add spring

---
+++ src/test/java/com/olexyn/owocal/owocal/OwocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olexyn.owocal.owocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
